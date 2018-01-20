7908766932af44170f6cf34263536f7518685b2e
StatueFungus <benedikt.suessmann@googlemail.com>
2018-01-20 12:40:46 +0100

    minor changes and new configurations

--- algorithm/SWalkAlgorithm.py
+++ algorithm/SWalkAlgorithm.py
@@ -13,6 +13,7 @@ class SWalkAlgorithm(AbstractCleaningAlgorithm):
         self.steps_between_lines = 0
         self.rotate_clockwise = False
         self.collision_after_direction_change = False
+        self.max_steps_between_lines = self._get_max_steps_between_lines()
 
     def update(self, obstacles, robot):
         super().update(obstacles, robot)
@@ -26,6 +27,7 @@ class SWalkAlgorithm(AbstractCleaningAlgorithm):
                 if self.collision_after_direction_change:
                     if randint(0, 1):
                         delta_angle = delta_angle * -1
+                    self.rotate_clockwise = delta_angle > 0
                     self.collision_after_direction_change = False
                 configuration_events.append(ConfigurationChanged(new_state=new_state, delta_angle=delta_angle))
                 self.state = State.MOVE_TO_NEXT_LINE
@@ -40,15 +42,17 @@ class SWalkAlgorithm(AbstractCleaningAlgorithm):
                 delta_angle = 180
                 configuration_events.append(ConfigurationChanged(new_state=new_state, delta_angle=delta_angle))
                 self.steps_between_lines = 0
+                self.max_steps_between_lines = self._get_max_steps_between_lines()
                 self.state = State.WALK_LINE
                 self.collision_after_direction_change = True
 
-            if self.steps_between_lines >= 7 :
+            if self.steps_between_lines >= self.max_steps_between_lines :
                 new_state = RobotState.ROTATE
                 delta_angle = self._get_current_angle()
                 configuration_events.append(ConfigurationChanged(new_state=new_state, delta_angle=delta_angle))
                 self.state = State.WALK_LINE
                 self.steps_between_lines = 0
+                self.max_steps_between_lines = self._get_max_steps_between_lines()
                 self.rotate_clockwise = not self.rotate_clockwise
 
             return configuration_events
@@ -57,6 +61,9 @@ class SWalkAlgorithm(AbstractCleaningAlgorithm):
 
     def _get_current_angle(self):
         return 90 if self.rotate_clockwise else -90
+
+    def _get_max_steps_between_lines(self):
+        return randint(2,7)
 
 
 class State(Enum):
